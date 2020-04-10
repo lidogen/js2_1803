d1a96ae2617fb6d619c499d48d14e6503419b769
Ostrovskaya Elena <ka-lenka@inbox.ru>
2020-04-10 19:09:57 +0300

    add server

--- students/Ostrovskaya Elena/Projects/src/public/components/search.vue
+++ students/Ostrovskaya Elena/Projects/src/public/components/search.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <form action="#" class="search-form" @input="$root.$children[0].$refs.catalog.setShowItems(search)">
+        <form action="#" class="search-form" @input="$parent.$refs.catalog.setShowItems(search)">
             <input type="text" class="search-field" v-model="search">
             <button class="btn-search">
                 <i class="fas fa-search"></i>
