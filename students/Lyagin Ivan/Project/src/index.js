e5fe79f65581ccd6ed908d15e459c074b83f2d9c
Niga9L <lyaginvanya@gmail.com>
2020-04-12 21:03:57 +0300

    Vue + Cli fill project

--- students/Lyagin Ivan/Project/src/index.js
+++
@@ -1,8 +0,0 @@
-import './public/style/style.css'
-import './public/style/normalize.css'
-
-import Vue from 'vue'
-import app from './public/index.vue'
-new Vue ({
-    render: h => h(app)
-}).$mount('#app');
