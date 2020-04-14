f7fdbf2525eeefc158258367f0f203fa54031ee9
Egorarefjev <38657241+Egorarefjev@users.noreply.github.com>
2020-04-14 21:29:54 +0300

    Create Writer.js

---
+++ students/Arefjev Egor/Project/server/Writer.js
@@ -0,0 +1,15 @@
+let fs = require('fs')
+
+module.exports = async function (file, data) {
+    let stat = true
+    try {
+        fs.writeFile(file, JSON.stringify(data, null, ' '), (err) => {
+            if(err) {
+                status = false
+            }
+        })
+    }
+    finally {
+        return stat
+    }
+}
